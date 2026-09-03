868b0120e92583683cc2f1ddeddda6b93864b8c6
Mannem23 <geethikamannem@gmail.com>
2026-09-03 15:39:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+// Simple welcome message
+
+console.log("Welcome to Geethika's Portfolio!");
+
+
+// Smooth scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(event) {
+
+        event.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        target.scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+
+});
